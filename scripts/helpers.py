fcef0e0aa2d7b4d3cb38c6608b5096ebfc91514d
Prateek Vachher <vachh007@umn.edu>
2019-08-01 16:38:50 -0700

    Bug Fix Test

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# from scripts import tabledef
-import tabledef
+from scripts import tabledef
+#import tabledef
 from flask import session
 from sqlalchemy.orm import sessionmaker
 from contextlib import contextmanager
