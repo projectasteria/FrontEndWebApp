993e37b4cc4c30f3d4195b541690fcf023d8c748
Prateek Vachher <vachh007@umn.edu>
2019-08-05 17:30:35 -0700

    Added Sleep in Tests

--- tests/test1.py
+++ tests/test1.py
@@ -1,9 +1,12 @@
+import time
 import urllib
 import requests
 from flask import Flask
 from flask_testing import TestCase
 
 url = 'http://localhost:5000'
+
+time.sleep(5)
 
 class MyTest(TestCase):
 
@@ -13,14 +16,17 @@ class MyTest(TestCase):
         return app
 
     def test_server_is_up_and_running(self):
+        print ("Test Started")
         response = (urllib.request.urlopen(url))
         self.assertEqual(response.code, 200)
     
     def test_server_correct(self):
+        print ("Test Started")
         response = urllib.request.urlopen(url)
         source = str(response.read()).split("\\n")
         self.assertEqual(source[0][2:], "<!-- LOGINPAGE ALIVE TEST -->")
     
     def test_login(self):
+        print ("Test Started")
         r = requests.post(url, data={"username":"test", "password":"test"})
         self.assertEqual(r.text, '{"status": "Login successful"}')
